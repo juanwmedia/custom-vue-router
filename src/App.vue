5f8683d5fac8fc3aaf346e82d77013e0c3aaf638
@juanwmedia <juan@wmedia.es>
2019-10-01 11:04:34 +0200

    Estado inicial

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <BlackCat imagen="cat1.jpg"/> 
+      <BlackCat imagen="cat2.jpg"/> 
+      <BlackCat imagen="cat3.jpg"/> 
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import BlackCat from './components/BlackCat.vue';
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    BlackCat
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  html, body {
+    height: 100%;
+    margin: 0;
+    padding: 0;
+  }
 </style>
---
+++ src/components/BlackCat.vue
@@ -0,0 +1,25 @@
+<template>
+  <div :style="{backgroundImage: `url(${require(`@/assets/${imagen}`)})`}">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BlackCat",
+  props: {
+    imagen: {
+      type: String,
+      required: true
+    }
+  },
+};
+</script>
+
+<style scoped>
+    div {
+        height: 100vh;
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+    }
+</style>
